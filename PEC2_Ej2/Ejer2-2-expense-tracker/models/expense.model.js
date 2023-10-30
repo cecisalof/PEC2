3362ca57416f05cc0123564155176dc12640fa44
Cecilia <cecilia.sanchezlosada@gmail.com>
2023-10-30 10:21:37 +0100

    [Fix]: Create a random id to every new transaction

--- PEC2_Ej2/Ejer2-2-expense-tracker/models/expense.model.js
+++ PEC2_Ej2/Ejer2-2-expense-tracker/models/expense.model.js
@@ -6,12 +6,21 @@
 
 class Expense {
     constructor({ text, amount }) {
+        this.id = this.uuidv4();
         console.log(amount);
         console.log(text);
         this.text = text,
-        this.amount = amount 
-
+        this.amount = amount
         console.log(this);
     }
-  }
-  
+
+    // Método que genera ID aleatorias
+    uuidv4() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (
+                c ^
+                (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+            ).toString(16)
+        );
+    }
+}
